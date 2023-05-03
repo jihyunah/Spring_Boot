f46328a882a74c5c80eefff0285fac2a0ef2855b
이지현 <121652350+jihyunah@users.noreply.github.com>
2023-05-03 11:25:10 +0900

    05.03.11

--- workspace/chapter01Graddle/src/main/java/com/example/demo/Chapter01GraddleApplication.java
+++ workspace/chapter01Graddle/src/main/java/com/example/demo/Chapter01GraddleApplication.java
@@ -3,12 +3,15 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.demo.controller","board.controller"})
 public class Chapter01GraddleApplication {
 
 	public static void main(String[] args) {
 		//SpringApplication.run(Chapter01GraddleApplication.class, args);
+		
 		SpringApplication springApplication = new SpringApplication(Chapter01GraddleApplication.class);
 		springApplication.setWebApplicationType(WebApplicationType.NONE);
 		springApplication.run(args);
