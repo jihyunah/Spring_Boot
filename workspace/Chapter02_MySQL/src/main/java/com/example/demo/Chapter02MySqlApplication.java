0f59ca5166623c22135b98e05c79463ad0b5c5fd
이지현 <121652350+jihyunah@users.noreply.github.com>
2023-05-03 15:14:00 +0900

    05.03.15

--- workspace/Chapter02_MySQL/src/main/java/com/example/demo/Chapter02MySqlApplication.java
+++ workspace/Chapter02_MySQL/src/main/java/com/example/demo/Chapter02MySqlApplication.java
@@ -2,8 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = {"main.controller", "user.controller", "user.service", "user.dao", "user.bean"})
+@ComponentScan(basePackages = {"main.controller", "user.*"})
 public class Chapter02MySqlApplication {
 
 	public static void main(String[] args) {
