5a84c3470c6ba774b6056071953ce8809f6b7004
이지현 <121652350+jihyunah@users.noreply.github.com>
2023-05-08 11:23:28 +0900

    05.08

--- workspace/Chapter03_JPA/src/main/java/com/example/demo/BoardDTO.java
+++ workspace/Chapter03_JPA/src/main/java/com/example/demo/BoardDTO.java
@@ -1,16 +1,22 @@
 package com.example.demo;
 
-import java.security.Timestamp;
+import java.sql.Timestamp;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.stereotype.Repository;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 
+@Entity //이 dto는 테이블과 바로 연결된거라고 알려주는 것.
 @Table(name = "board") //얘네를 board 테이블로 쓰겠다. 워크벤치에서 따로 테이블 안 만들어도 됨.
+@Data
 public class BoardDTO {
 	
+	@Id //이 Column이 primary key인 것을 알려준다.
 	@Column(name = "id", nullable = false, unique = true, length = 30) //null값 허용 X, 공통 안되게, 길이 30 
 	private String id;
 	
