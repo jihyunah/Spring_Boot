bee5af9b7d91922c7d40af29d2441eb0185335b9
이지현 <121652350+jihyunah@users.noreply.github.com>
2023-05-09 11:24:43 +0900

    05.09

--- workspace/Chapter04_JPA/src/main/java/user/bean/UserDTO.java
+++ workspace/Chapter04_JPA/src/main/java/user/bean/UserDTO.java
@@ -1,10 +1,23 @@
 package user.bean;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
+@Entity
+@Table(name = "usertable")
 @Data
 public class UserDTO {
+	@Column(name = "name", nullable = false, length = 30) //null값 허용 X
 	private String name;
+	
+	@Id //primary key
+	@Column(name = "id", length = 30)
 	private String id;
+	
+	@Column(name = "pwd", nullable = false, length = 30)
 	private String pwd;
+	
 }
--- workspace/Chapter04_JPA/src/main/java/user/controller/UserController.java
+++ workspace/Chapter04_JPA/src/main/java/user/controller/UserController.java
@@ -1,6 +1,8 @@
 package user.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,11 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import user.bean.UserDTO;
+import user.service.UserService;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
-	
+	@Autowired
+	private UserService userService;
 	
 	@GetMapping("writeForm")
 	public String writeForm() {
@@ -22,6 +26,6 @@ public class UserController {
 	@PostMapping("write")
 	@ResponseBody //무조건 jsp를 찾아간다. 그래서 이거를 써야함.
 	public void write(@ModelAttribute UserDTO userDTO) {
-		
+		userService.write(userDTO);
 	}
 }
